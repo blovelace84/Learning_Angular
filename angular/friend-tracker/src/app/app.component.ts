959eb9bf7be744d6164eee3a78b9cc0ceca5d837
Shado <blovelace84@gmail.com>
2025-04-16 20:07:33 -0400

    commit all

---
+++ angular/friend-tracker/src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { ProfileInfoComponent } from "./profile-info/profile-info.component";
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, ProfileInfoComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'friend-tracker';
+}
---
+++ angular/friend-tracker/src/app/profile-info/profile-info.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'abccdefg',
+  imports: [],
+  templateUrl: './profile-info.component.html',
+  styleUrl: './profile-info.component.css'
+})
+export class ProfileInfoComponent {
+
+}
