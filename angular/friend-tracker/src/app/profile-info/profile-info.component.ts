39a3d6ae53a1a2a4b98d9f8703a12d8ea8cbc4cc
Shado <blovelace84@gmail.com>
2025-04-17 07:55:17 -0400

    commit all

--- angular/friend-tracker/src/app/profile-info/profile-info.component.ts
+++ angular/friend-tracker/src/app/profile-info/profile-info.component.ts
@@ -7,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrl: './profile-info.component.css'
 })
 export class ProfileInfoComponent {
-
+  name = "Brittani Lovelace";
+  profilePictureUrl = "assets/Brittani headshots.jpg";
+  age = 32;
+  bio = "I like to play video games and I am passionate about computers and coding.";
+  birthday = "February 27, 1993";
+  interests = [
+    "Video Games",
+    "Coding",
+    "Technology(computers)",
+  ]
 }
